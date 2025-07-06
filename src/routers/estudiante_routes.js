a04a6094137fd8870eba4c6febb116ff3c02bb5b
PatricioPonce28 <geanotoponce95@gmail.com>
2025-07-05 22:16:20 -0500

    Función IA

--- src/routers/estudiante_routes.js
+++ src/routers/estudiante_routes.js
@@ -1,9 +1,11 @@
 import {Router} from 'express'
-import { completarPerfil  } from '../controllers/estudiante_controllers.js'
+import { completarPerfil, chatEstudiante   } from '../controllers/estudiante_controllers.js'
 import {verificarTokenJWT} from '../middlewares/JWT.js'
 
 const router = Router()
 
 router.put('/perfil/completar', verificarTokenJWT, completarPerfil)
+
+router.post('/perfil/chat', verificarTokenJWT, chatEstudiante);
 
 export default router 
